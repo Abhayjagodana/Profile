b43433030a7258d8c4a8fed13b64df12c08996ea
Abhi <jagodanabhay@gmail.com>
2025-07-20 15:13:40 +0530

    first commit

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -9,13 +9,10 @@ function About() {
       <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2">
         {/* About Box */}
         <div className="bg-white shadow-lg rounded-xl p-6 md:p-10">
-          <h1 className="text-2xl sm:text-3xl font-bold mb-4 text-gray-800">About Me</h1>
-          <p className="text-sm sm:text-base text-gray-600 leading-relaxed">
-            Hello! I'm <span className="font-semibold text-blue-600">Abhi Jagodana</span>, a frontend developer 
-          </p>
-          {/* <p className="text-sm sm:text-base text-gray-600 mt-4 leading-relaxed">
-            I enjoy working with modern tools like <strong>React</strong> and <strong>Tailwind CSS</strong>. Always learning, always building.
-          </p> */}
+          <h1 className="text-2xl sm:text-3xl font-bold mb-4 text-gray-800">
+  About Abhi Jagodana – Frontend Developer
+</h1>
+
         </div>
 
         <Helmet>
